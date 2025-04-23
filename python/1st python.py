54a77486b591d9357f625533e1a459bc7175b7c2
Abdurahman Aliyi <abdurahmanali693@gmail.com>
2025-04-23 14:58:11 +0300

    Initial commit

---
+++ python/1st python.py
@@ -0,0 +1,15 @@
+
+char_name="IBRAHIM"
+int_age="378"
+print("There is once a man whose name is " + char_name +" ")
+print("he was "+int_age+ " years old ")
+char_name="Abdure "
+print("he really liked his name  " + char_name +" ")
+print("but not being "+int_age+ "")
+sami="sami fi daniiin obbolewwan waliiti "
+dani= "ABBA MALA NAMA LAMA JEDHAMA MII REE"
+print(len(dani))
+print(sami[-1])
+print(dani.replace("ABBA", "NAMA"))
+
+
---
+++ python/prac1.py
@@ -0,0 +1,50 @@
+"""print("hello musa")
+print("how to be a programmer")
+first_saba = "KeyErro"
+last_saba= "shuna"
+whole_saba= first_saba+"  "+ last_saba
+print(type(first_saba))
+print(whole_saba)
+print(first_saba +"tuma")
+age = 45
+age +=3
+print(age)
+hieght =34.5
+print("your hieght is"+"     "  +str(hieght))
+print(type(hieght))
+bro, bra, bre= "90","war",44
+print(bre,bro)
+name="abdurahman"
+print(len(name))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.upper())
+name="abdurahman"
+print(name.isalpha())
+print(name.count("a"))
+print(name.replace("a","e"))
+print(name*2
+x= 1 #int
+y=2.9 #float
+z="45" #str
+x=int(x)
+
+print("z is  "+z)
+print(x*100)
+print(y*4)
+name=input("what is your name?: ")
+age=int(input("how old are you?: "))
+height=float(input("how tall are you?: "))
+
+print("how are you "+name)
+print("you are "+ str(age) +" years old and your height is "+str(height)+" inch" )"""
+import math
+pi =3.14
+x=3
+t=9
+print(max (pi ,x,t))
+print(pow(x,5))
+print(math.sqrt(t))
+
